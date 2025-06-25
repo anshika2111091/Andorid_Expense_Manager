3b65077937ed9d2fb59570b0e2cb071996184d39
Anshikapuri <anshika.puri@revfin.in>
2025-06-25 14:40:04 +0530

    added_navigation_bars

--- app/src/main/java/com/example/expensemanager/MainActivity.java
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.expensemanager;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +9,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.expensemanager.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.materialToolbar);
+        getSupportActionBar().setTitle("Transactions");
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
